74abc3f50b2cf492226875f999e58f7a98b2d315
Nitin Singh 🎓 | | B.Tech in AI & ML (1st Year) | |  🚀 Building projects in Python & learning DSA in C++ | | WEB DEVELOPMENT HTML,CSS 🤖 Passionate about AI, ML & real-world applications 📫 <nitinsinghbagri005@gmail.com>
2025-11-02 11:59:37 +0530

    Add files via upload

---
+++ LEARN JAVASCRIPT/DAY 2/node.js
@@ -0,0 +1,36 @@
+console.log("Hey this is tutorial of day 2 of js  ")
+
+let a = 7;
+let b = 5;
+let c = "Nitin";
+console.log(a + b)
+// console.log(typeof a ,typeof b, typeof c)
+
+const a1 = 6;
+// a1 = a1 + 5; // not allowed
+{
+    let a = 21;
+    console.log(a )
+
+}
+console.log(a)
+
+let x = 45; 
+let y = 32.65; 
+let z = "Nitin Singh"; 
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(typeof x, typeof y,typeof z,typeof p, typeof q,typeof r)
+
+
+
+let o = {
+    name : "Nitin",
+    Course : "B.Tech ",
+    "student id " : "250125"
+}
+console.log(o)
+o.branch = "AI & ML";
+console.log(o)
