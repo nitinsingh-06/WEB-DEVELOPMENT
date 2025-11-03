9ec9b719c1f59e71c8d1dcd88c21bd05b38b5f95
Nitin Singh 🎓 | | B.Tech in AI & ML (1st Year) | |  🚀 Building projects in Python & learning DSA in C++ | | WEB DEVELOPMENT HTML,CSS 🤖 Passionate about AI, ML & real-world applications 📫 <nitinsinghbagri005@gmail.com>
2025-11-03 23:19:00 +0530

    Add files via upload

---
+++ LEARN JAVASCRIPT/DAY 3/index.js
@@ -0,0 +1,28 @@
+function nice(name) {
+
+    
+    console.log("Hey " + name + " You are good! ")
+    console.log("Hey " + name + " You are nice!")
+    console.log("Hey " + name + " You are nothing! ")
+    console.log("Hey " + name + " How i can Help you today ")
+}
+
+
+nice("Virat")
+
+
+
+function sum(a,b){
+    // console.log(a+b)
+    return a+b
+}
+result = sum(3,5)
+console.log("The Sum Of a and b is :",result)
+
+
+const func1 = (x)=>{
+    console.log("This is func1 and value is ",x)
+}
+func1(45)
+func1(25)
+func1(34)
---
+++ LEARN JAVASCRIPT/DAY 3/node.js
@@ -0,0 +1,12 @@
+console.log("This is String Learning steps")
+
+let a = "Nitin";
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a.length)
+let friend = "Virat";
+
