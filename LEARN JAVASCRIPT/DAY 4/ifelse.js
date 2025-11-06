191ec38524b9ceca1adf6cb46f3e09caaa057953
Nitin Singh 🎓 | | B.Tech in AI & ML (1st Year) | |  🚀 Building projects in Python & learning DSA in C++ | | WEB DEVELOPMENT HTML,CSS 🤖 Passionate about AI, ML & real-world applications 📫 <nitinsinghbagri005@gmail.com>
2025-11-06 16:45:05 +0530

    Add files via upload

---
+++ LEARN JAVASCRIPT/DAY 4/ifelse.js
@@ -0,0 +1,27 @@
+console.log("Hello I am condition Tutorial : ")
+
+let age = 19;
+if(age>18){
+    console.log("You Can drive");
+
+}
+
+else{
+    console.log("You Can't Drive : ")   
+}
+
+
+a = 3;
+b = "3"
+if(a===b){
+    console.log("true")
+}
+else{
+    console.log("False")
+}
+
+
+x = 4;
+y = 6;
+let z = x>y ? (x-y) : (y-x);
+console.log(z)
