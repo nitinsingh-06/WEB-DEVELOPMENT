5a041a9c028e68c57433fc0933a65d5c3f4f3d9c
Nitin Singh 🎓 | | B.Tech in AI & ML (1st Year) | |  🚀 Building projects in Python & learning DSA in C++ | | WEB DEVELOPMENT HTML,CSS 🤖 Passionate about AI, ML & real-world applications 📫 <nitinsinghbagri005@gmail.com>
2025-11-06 17:28:27 +0530

    Add files via upload

---
+++ LEARN JAVASCRIPT/DAY 4/loops.js
@@ -0,0 +1,30 @@
+console.log("This is Loop tutorial ")
+
+// let a = 1;
+// for(let i = 0; i<=100;i++){
+//     console.log(i+1);
+// }
+
+
+
+
+// let obj = {
+//     namee: "Nitin",
+//     role: "Student",
+//     company: "GRD IMT"
+// }
+// for (const key in obj){
+//     const element = obj[key];
+//     console.log(key,element);
+// }
+
+// for ( const c of "Nitin"){
+//     console.log(c)
+// }
+
+
+let i = 1;
+while(i<6){
+    console.log(i);
+    i++;
+}
